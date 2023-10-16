1da39c9cd2315673a36e22317d1ab200d4dd0323
harry615 <70017280+harry615@users.noreply.github.com>
2023-10-16 14:47:20 +0100

    Update pf.py
    Update parameter tuning

--- src/pf_localisation/pf.py
+++ src/pf_localisation/pf.py
@@ -68,9 +68,9 @@ class PFLocaliser(PFLocaliserBase):
                     break
 
             new_particle = Pose()
-            new_particle.position.x = selected_particle.position.x + gauss(0, 0.15)
-            new_particle.position.y = selected_particle.position.y + gauss(0, 0.15)
-            new_particle.orientation = rotateQuaternion(selected_particle.orientation, gauss(0, 0.15))
+            new_particle.position.x = selected_particle.position.x + gauss(0, 0.25)
+            new_particle.position.y = selected_particle.position.y + gauss(0, 0.25)
+            new_particle.orientation = rotateQuaternion(selected_particle.orientation, gauss(0, 0.25))
 
             new_particle_cloud.poses.append(new_particle)
 
