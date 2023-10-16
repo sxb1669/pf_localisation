39a8e4524a5db13424f53f7436893ccdac504e04
harry615 <70017280+harry615@users.noreply.github.com>
2023-10-16 14:01:40 +0100

    Update pf.py
    Fine tuning parameters

--- src/pf_localisation/pf.py
+++ src/pf_localisation/pf.py
@@ -24,8 +24,8 @@ class PFLocaliser(PFLocaliserBase):
         for _ in range(num_particles):
             particle = Pose()
 
-            particle.position.x = initialpose.pose.pose.position.x + gauss(0, 0.3)  # Adjust noise parameters as needed
-            particle.position.y = initialpose.pose.pose.position.y + gauss(0, 0.3)  # Adjust noise parameters as needed
+            particle.position.x = initialpose.pose.pose.position.x + gauss(0, 0.1)  # Adjust noise parameters as needed
+            particle.position.y = initialpose.pose.pose.position.y + gauss(0, 0.6)  # Adjust noise parameters as needed
 
             initial_yaw = getHeading(initialpose.pose.pose.orientation)
             particle.orientation = rotateQuaternion(initialpose.pose.pose.orientation, gauss(0, 0.2))  # Adjust noise parameters as needed
